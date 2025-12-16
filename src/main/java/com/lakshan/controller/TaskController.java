3395985ec9c4084dd17bb17381b63aa2bb98df0f
Lakshan <lakshan.imantha02@gmail.com>
2025-12-16 22:38:40 +0530

    Add CrossOrigin annotation to avoid CORS issues

--- src/main/java/com/lakshan/controller/TaskController.java
+++ src/main/java/com/lakshan/controller/TaskController.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/tasks")
+@CrossOrigin(origins = "http://localhost:5173")
 public class TaskController {
 
     private final TaskService taskService;
