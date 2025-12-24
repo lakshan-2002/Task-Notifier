0d215d5641d2820ceab7d3ac9ab113cde83b083f
Lakshan <lakshan.imantha02@gmail.com>
2025-12-24 12:21:31 +0530

    Extract the email handling logic to a new DailyEmailScheduler class which handles sending email notifications to all available users.

--- src/main/java/com/lakshan/service/TaskService.java
+++ src/main/java/com/lakshan/service/TaskService.java
@@ -2,28 +2,19 @@ package com.lakshan.service;
 
 import com.lakshan.entity.Task;
 import com.lakshan.repository.TaskRepository;
-import com.lakshan.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
 import java.util.List;
 
 @Service
 public class TaskService {
 
     private final TaskRepository taskRepository;
-    private final UserRepository userRepository;
-    private final EmailService emailService;
 
     @Autowired
-    public TaskService(TaskRepository taskRepository,
-                       UserRepository userRepository,
-                       EmailService emailService
-    ) {
+    public TaskService(TaskRepository taskRepository) {
         this.taskRepository = taskRepository;
-        this.userRepository = userRepository;
-        this.emailService = emailService;
     }
 
     public void addNewTask(Task task) {
@@ -35,47 +26,10 @@ public class TaskService {
     }
 
     public List<Task> getTasksByUserId(int userId) {
-        List<Task> tasks = taskRepository.findByUserId(userId);
-        if (tasks.isEmpty())
+        if (!(taskRepository.findByUserId(userId).isEmpty()))
+            return taskRepository.findByUserId(userId);
+        else
             throw new IllegalArgumentException("No tasks found for user with id: " + userId);
-
-        List<Task> tasksDueToday = filterTasksDueToday(tasks, LocalDate.now());
-        if (!tasksDueToday.isEmpty())
-            getEmailDetails(userId, tasksDueToday);
-
-        return tasks;
-    }
-
-    private List<Task> filterTasksDueToday(List<Task> tasks, LocalDate date) {
-        return tasks.stream()
-                .filter(t -> date.equals(t.getDueDate()))
-                .toList();
-    }
-
-    private void getEmailDetails(int userId, List<Task> tasksDueToday) {
-        String recipientEmail = userRepository.findById(userId).orElseThrow(() ->
-                new IllegalArgumentException("User not found with id: " + userId)
-        ).getEmail();
-
-        String subject = "Tasks Due Today Reminder";
-        String finalBody = getFullBody(tasksDueToday);
-
-        emailService.sendEmail(recipientEmail, subject, finalBody);
-    }
-
-
-    private String getFullBody(List<Task> tasksDueToday) {
-        StringBuilder body = new StringBuilder();
-        for (Task task : tasksDueToday) {
-            body.append("Title: ").append(task.getTitle()).append("\n")
-                    .append("Description: ").append(task.getDescription()).append("\n")
-                    .append("Priority: ").append(task.getPriority()).append("\n\n");
-        }
-
-        return "Dear User,\n\nThis is a reminder that the following tasks are due today:\n\n"
-                + body +
-                "\nPlease make sure to complete them on time." +
-                "\n\nBest regards,\nTaskNotifier System";
     }
 
     public Task getTaskById(int id) {
